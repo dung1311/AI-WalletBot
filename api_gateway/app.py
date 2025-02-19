81f42fef549bbf73a8814963c29708030b443ad7
dung1311 <109650915+dung1311@users.noreply.github.com>
2025-02-19 21:57:14 +0700

    something

--- api_gateway/app.py
+++ api_gateway/app.py
@@ -12,29 +12,29 @@ load_dotenv()
 
 app = FastAPI()
 
-@app.middleware("http")
-async def authenticate(req: Request, next):
-    token = req.headers.get("Authorization")
-    if not token:
-        return Response(status_code=400, content=json.dumps({
-            "code": "400",
-            "message": "Token is required",
-            "metadata": None
-        }))
-    # remove Bearer
-    token = token.split(" ")[-1]
+# @app.middleware("http")
+# async def authenticate(req: Request, next):
+#     token = req.headers.get("Authorization")
+#     if not token:
+#         return Response(status_code=400, content=json.dumps({
+#             "code": "400",
+#             "message": "Token is required",
+#             "metadata": None
+#         }))
+#     # remove Bearer
+#     token = token.split(" ")[-1]
 
-    try:
-        decodeUser = jwt.decode(token, os.getenv("JWT_SECRET_ACCESS"), algorithms=["HS256"])
-        req.state.user = decodeUser
+#     try:
+#         decodeUser = jwt.decode(token, os.getenv("JWT_SECRET_ACCESS"), algorithms=["HS256"])
+#         req.state.user = decodeUser
 
-        return await next(req)
-    except InvalidTokenError:
-        return Response(status_code=400, content=json.dumps({
-            "code": "400",
-            "message": "Invalid token",
-            "metadata": None
-        }))
+#         return await next(req)
+#     except InvalidTokenError:
+#         return Response(status_code=400, content=json.dumps({
+#             "code": "400",
+#             "message": "Invalid token",
+#             "metadata": None
+#         }))
 
 @app.get("/")
 def index():
@@ -52,11 +52,12 @@ async def askAI(req: Request):
         }))
     
     response = await ask(query)
-    return Response(status_code=200, content=json.dumps({
+    json_response = json.loads(response)
+    return {
         "code": 200,
-        "message": "Recieve response success",
-        "metadata": response
-    }, ensure_ascii=False))
+        "message": "Recieved response",
+        "metadata": json_response
+    }
     
 @app.get("/call")
 async def call(req: Request):
--- chatbot_service/app.py
+++ chatbot_service/app.py
@@ -6,9 +6,22 @@ async def ask(query: str):
                 'role': 'system', 
                 'content': 'You only understand Vietnamese, say \"Tôi không hiểu\" if there are someone speak language which is not Vietnamese. Response in Vietnamese'
             }
-    response = await AsyncClient().chat(model='llama3.2:3b', messages=[promt, {
+    test = {
+        'role': 'system',
+        'content': '''
+        Please extract the following information from the given text and return it as a JSON object:
+
+        amount: the amount of money in the text. Return number only, example: 1000, 1000000, 1000000000
+        description: copy the given text
+        category: the category of the given text. Return string only, example: 'food', 'drink', 'transport', 'shopping', 'entertainment', 'health', 'education', 'other'
+        advice: the advice for the amount of money in the text. Return string only. always complain about the money spent. Translate into Vietnamese
+
+        Return amount, description, category, advice in a JSON object. Remember only response in Vietnamese
+    '''
+    }
+    response = await AsyncClient().chat(model='qwen2.5:7b', messages=[test, {
                   'role': 'user', 
                   'content': f'{query}'
-            }])
+            }], format='json')
     print("finish!!!")
     return response.message.content
